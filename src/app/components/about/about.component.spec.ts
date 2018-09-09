8699993765a4ab89c7b5b54f2203c57a062636f8
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-09-09 20:58:49 +0530

    unit tests for product-list component product-list service about component

--- src/app/components/about/about.component.spec.ts
+++ src/app/components/about/about.component.spec.ts
@@ -1,25 +1,67 @@
+import { SharedModule } from './../../shared/shared.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
 
-describe('AboutComponent', () => {
+fdescribe('AboutComponent', () => {
   let component: AboutComponent;
+  let element: any;
   let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      declarations: [ AboutComponent ],
+      imports: [SharedModule]
     })
-    .compileComponents();
+    .compileComponents(); // JIT html to JS
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
+    
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    element = fixture.nativeElement;
+    expect(element.querySelectorAll('li').length)
+                                      .toBe(2);
   });
+
+
+  it('should clear all li when toggle', () => {
+    expect(component).toBeTruthy();
+    element = fixture.nativeElement;
+    expect(element.querySelectorAll('li').length).toBe(2);
+    
+    component.showMembers = false;
+
+    fixture.detectChanges();
+    expect(element.querySelectorAll('li').length).toBe(0);
+  })
+
+
+  it('should create add member', () => {
+    expect(component).toBeTruthy();
+    element = fixture.nativeElement;
+    component.addMember('Nila');
+    fixture.detectChanges();
+
+    expect(element.querySelectorAll('li').length).toBe(3);
+  
+    expect(element.querySelectorAll('li')[2].textContent)
+          .toContain('Nila');
+  })
+
+
+  it('should empty list', () => {
+    expect(component).toBeTruthy();
+    element = fixture.nativeElement;
+    component.empty();
+    fixture.detectChanges();
+
+    expect(element.querySelectorAll('li').length).toBe(0);
+   });
 });
--- src/app/product/components/product-list/product-list.component.spec.ts
+++ src/app/product/components/product-list/product-list.component.spec.ts
@@ -1,14 +1,52 @@
+import { FormsModule } from '@angular/forms';
+import { CartModule } from './../../../cart/cart.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductListComponent } from './product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from '../../services/product.service';
+import { RouterModule } from '@angular/router';
 
-describe('ProductListComponent', () => {
+import { APP_BASE_HREF } from '@angular/common';
+import { Observable, of } from 'rxjs';
+import { Product } from '../../models/product';
+
+import { ProductListComponent } from '../product-list/product-list.component';
+import { SharedModule } from '../../../shared/shared.module';
+
+fdescribe('ProductListComponent', () => {
   let component: ProductListComponent;
   let fixture: ComponentFixture<ProductListComponent>;
+  let productService: ProductService;
+  let element: any;
+
+  class ProductMockService {
+    constructor() {
+      console.log('***MOCK SERVICE CREATED');
+    }
+
+    getProducts(): Observable<Product[]>  {
+      console.log('***MOCKED SERVICE ');
+      return of(<Product[]> [{id: 1, name: 'Nexus Phone', price: 10000, year: 2018}]);
+    }
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductListComponent ]
+      imports: [
+        HttpClientModule,
+        RouterModule.forRoot([]),
+        SharedModule,
+        CartModule,
+        FormsModule
+      ],
+      declarations: [ ProductListComponent ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'},
+        {
+          provide: ProductService,
+          useClass: ProductMockService
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +54,34 @@ describe('ProductListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductListComponent);
     component = fixture.componentInstance;
+
+    productService = fixture.debugElement.injector.get(ProductService);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+    element = fixture.nativeElement;
+
+    expect(element.querySelectorAll('tr')[1].textContent)
+          .toContain('NEXUS PHONE');
+
+    
+    expect(element.querySelectorAll('tr')[1].textContent)
+    .toContain('10,000.00');
+
+
+    expect(element.querySelectorAll('tr')[1]
+                   .querySelectorAll('td')[1] // price column
+                   .textContent)
+                  .toContain('10,000.00');
+
+    expect(element.querySelectorAll('tr')[1]
+    .querySelectorAll('td')[2] // year column
+    .textContent)
+    .toContain('2018');
+
   });
 });
